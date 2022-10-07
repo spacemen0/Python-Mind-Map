7e1903ac94c8d41ec3d6b1ee6c715d70953660f3
clpclp-sub <1300791830@qq.com>
2022-10-07 20:30:59 +0800

    vue打包 dist下

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -14,5 +14,29 @@ module.exports = defineConfig({
                 }
             }
         }
+    },
+    assetsDir: 'static',
+    parallel: false,
+    publicPath: './',
+    outputDir: 'dist',
+
+    configureWebpack: config => {
+        // 不是开发环境时生效
+        if (process.env.NODE_ENV !== 'development') {
+            config['performance'] = {
+                // 警告 webpack 的性能提示
+                hints: 'warning',
+                // 入口起点的最大体积
+                maxEntrypointSize: 50000000,
+                // 生成文件的最大体积
+                maxAssetSize: 30000000,
+                // 只给出 js 文件的性能提示
+                assetFilter: function (assetFilename) {
+                    return assetFilename.endsWith('.js')
+                }
+            }
+        }
     }
+
+
 })
