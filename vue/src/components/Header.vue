1e594ddfecc5f0955bf40ec126867a4af7eeb623
clpclp-sub <1300791830@qq.com>
2023-06-10 00:40:20 +0800

    导入学生账户

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
-    <el-menu :default-active="'activeIndex'" router class="el-menu-demo" style="border-bottom-color: #5c5e63;" mode="horizontal"
+    <el-menu :default-active="'activeIndex'" router class="el-menu-demo" style="border-bottom-color: #5c5e63;"
+             mode="horizontal"
              @select="" background-color="#37373c" active-text-color="#f6f4ef" text-color="rgb(184 184 184)">
         <el-menu-item disabled style="float: left; opacity: 1; cursor: auto; font-size: 20px">AI课堂</el-menu-item>
         <!--<el-menu-item disabled style="opacity: 1; cursor: auto">{{ User.name }}, {{ User.userID }}</el-menu-item>-->
@@ -13,16 +14,18 @@
 
         <el-menu-item index="/personal">{{ User.userID === '100000000000' ? '班级情况' : '个人情况' }}</el-menu-item>
 
-<!--        <el-menu-item index="/ranking">班级排名</el-menu-item>-->
+        <!--        <el-menu-item index="/ranking">班级排名</el-menu-item>-->
 
-<!--        <el-menu-item index="/gragh">每周题数</el-menu-item>-->
+        <!--        <el-menu-item index="/gragh">每周题数</el-menu-item>-->
 
         <!--<el-menu-item index="/answer">答题界面</el-menu-item>-->
         <el-menu-item index="/main">知识图谱</el-menu-item>
 
         <!--管理员功能区-->
         <el-menu-item index="/studentInfoList" v-if="this.User.isAdmin === true">学生信息</el-menu-item>
-        <el-button class="btn" type="info" v-if="this.User.isAdmin === true" @click="()=>{this.uploadDialog = true}">导入学生账户</el-button>
+        <el-button class="btn" type="info" v-if="this.User.isAdmin === true" @click="()=>{this.uploadDialog = true}">
+            导入学生账户
+        </el-button>
         <el-dialog :visible.sync="uploadDialog" width="20%" top="25vh" :modal="false" :modal-append-to-body="false">
             <el-upload class="upload-demo" drag :limit="1" :file-list="fileList" show-file-list :auto-upload="true"
                        action="#" :on-change="handleChange" :http-request="upload">
@@ -37,7 +40,7 @@
 
 <script>
 import request from "@/utils/request";
-
+const FormData = require('form-data')
 export default {
     name: "Header",
     inject: ['reload'],
@@ -67,10 +70,16 @@ export default {
         handleChange(file, fileList) {
             this.fileList = fileList;
         },
-        upload() {
+        upload(item) {
+            this.$message('文件上传中........')
+            let data = new FormData();
+            data.append('students', this.fileList[0].raw);
             request.post(
                 '/admin/importstudents',
-                {file: this.fileList[0].raw}
+                data,
+                {
+                    headers: {"Content-Type": "multipart/form-data"}
+                }
             ).then(res => {
                 if (res.status === 200) {
                     this.$message({
@@ -115,14 +124,15 @@ export default {
     padding-left: 10%;
 }
 
-.el-menu--horizontal>.el-submenu,
+.el-menu--horizontal > .el-submenu,
 .el-menu--horizontal > .el-menu-item {
     float: right;
     font-size: 16px;
     padding-left: 15px;
     padding-right: 15px;
 }
-/deep/.el-submenu__title {
+
+/deep/ .el-submenu__title {
     font-size: 16px;
     color: ghostwhite;
 }
